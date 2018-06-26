149a66e6339526ee0ee0a881f030000819553718
sadba86@gmail.com <sadba86@gmail.com>
2018-06-26 12:10:48 +0000

    ajout des tab pour abscences

--- app/src/main/java/com/lab/sadba/loginparent/AbscenceActivity.java
+++ app/src/main/java/com/lab/sadba/loginparent/AbscenceActivity.java
@@ -1,19 +1,29 @@
 package com.lab.sadba.loginparent;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.lab.sadba.loginparent.Adapter.ViewPagerAdapter;
+import com.lab.sadba.loginparent.Fragments.AbscenceFragment;
+import com.lab.sadba.loginparent.Fragments.RetardFragment;
+
 public class AbscenceActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abscence);
 
+        tabLayout = findViewById(R.id.tablayout_id);
+        viewPager = findViewById(R.id.viewpager_id);
 
         toolbar =  findViewById(R.id.toolbar_emploi);
 
@@ -31,5 +41,21 @@ public class AbscenceActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        setUpViewPager(viewPager);
+    }
+
+    private void setUpViewPager(final ViewPager viewPager){
+
+
+        Bundle bundle = new Bundle();
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), bundle, this);
+
+        //ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), bundle, this);
+        adapter.AddFragment(new AbscenceFragment(), "ABSCENCES");
+        adapter.AddFragment(new RetardFragment(), "RETARDS");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
